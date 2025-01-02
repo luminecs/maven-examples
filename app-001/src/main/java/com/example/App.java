367d1b1ff4d02cb8670578adf70c822316070917
luminecs <wangxuke.china@gmail.com>
2025-01-02 17:17:48 +0800

    @FunctionalInterface

--- app-001/src/main/java/com/example/App.java
+++ app-001/src/main/java/com/example/App.java
@@ -5,6 +5,29 @@ package com.example;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String text = "Hello World!";
+
+        StringProcessor toUpperCaseProcessor = input -> input.toUpperCase();
+
+        StringProcessor reverseProcessor = input -> new StringBuilder(input).reverse().toString();
+
+        String upperCaseResult = applyProcessor(text, toUpperCaseProcessor);
+        String reverseResult = applyProcessor(text, reverseProcessor);
+
+        System.out.println("Original text: " + text);
+        System.out.println("Upper case text: " + upperCaseResult);
+        System.out.println("Reversed text: " + reverseResult);
+        System.out.println("Upper case text: " + toUpperCaseProcessor.process(text));
+        System.out.println("Reversed text: " + reverseProcessor.process(text));
     }
+
+    public static String applyProcessor(String input, StringProcessor processor) {
+        return processor.process(input);
+    }
+}
+
+// 可以不使用注解
+@FunctionalInterface
+interface StringProcessor {
+    String process(String input);
 }
